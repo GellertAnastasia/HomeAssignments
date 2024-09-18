5d743560e8deaf0445da7f63a3977a72e7c0d094
GellertAnastasia <st130144@student.spbu.ru>
2024-09-19 00:34:10 +0300

    Добавление файлов: реализация функции main, реализация функции приветствия, заголовочный файл

---
+++ hello.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+string name="world";
+
+void hello()
+{
+	cout<<"Hello, "<<name<<"!"<<endl;
+	cin>>name;
+}
---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include "header.h"
+
+using namespace std;
+
+extern string name;
+
+int main()
+{
+	while(true)
+	{
+		hello();
+	}
+}
