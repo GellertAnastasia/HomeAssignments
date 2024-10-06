afd641e93f0caad6c1d6e275b1a996796a1c3dda
GellertAnastasia <st130144@student.spbu.ru>
2024-10-06 14:01:52 +0300

    adding comments

--- Assignment2/Assignment2a/main.cpp
+++ Assignment2/Assignment2a/main.cpp
@@ -9,22 +9,22 @@
 
 int main()
 {
-	std::ifstream infile;
-	infile.open("source",std::ios::binary|std::ios::in); //открываем файл в бинарном виде
-	int size = std::filesystem::file_size("source") - 1; //находим размер файла
-	char* arr = new char[size]; //создаем динамический массив
-	infile.read(arr,size); //считываем файл в массив
+    std::ifstream infile;
+    infile.open("source",std::ios::binary|std::ios::in); //открываем файл в бинарном виде
+    int size = std::filesystem::file_size("source") - 1; //находим размер файла
+    char* arr = new char[size]; //создаем динамический массив
+    infile.read(arr,size); //считываем файл в массив
 
-	reverse(arr, size); //переворачиваем массив
+    reverse(arr, size); //переворачиваем массив
 
-	std::ofstream outfile;
-        outfile.open("temppdf",std::ios::binary|std::ios::out); //открываем\создаем файл вывода
-	outfile.write(arr,size); //записываем массив в файл
+    std::ofstream outfile;
+    outfile.open("temppdf",std::ios::binary|std::ios::out); //открываем\создаем файл вывода
+    outfile.write(arr,size); //записываем массив в файл
 
-	delete [] arr; //освобождаем память
+    delete [] arr; //освобождаем память
 
-	infile.close(); //закрываем файлы
-	outfile.close();
+    infile.close(); //закрываем файлы
+    outfile.close();
 
-	return 0;
+    return 0;
 }
--- Assignment2/Assignment2a/reverse.cpp
+++ Assignment2/Assignment2a/reverse.cpp
@@ -6,12 +6,12 @@
 
 void reverse(char arr[], int size)
 {
-	int i;
-	char x;
-	for(i = 0; i < size/2; i++)
-        {
-               	x = arr[i];
-               	arr[i] = arr[size - i - 1];
-               	arr[size - i - 1] = x;
-	}
+    int i;
+    char x;
+    for(i = 0; i < size/2; i++)
+    {
+        x = arr[i];
+	arr[i] = arr[size - i - 1];
+       	arr[size - i - 1] = x;
+    }
 }
