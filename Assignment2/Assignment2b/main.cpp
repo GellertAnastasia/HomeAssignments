8bd46b9b15ed608697a38f887457b3930867f6d4
GellertAnastasia <st130144@student.spbu.ru>
2024-10-08 00:14:31 +0300

    creating makefile and adding comments

--- Assignment2/Assignment2b/main.cpp
+++ Assignment2/Assignment2b/main.cpp
@@ -1,43 +1,42 @@
+/* Anastasia Gellert st130144@student.spbu.ru
+	assignment 2b
+*/
+
 #include <iostream>
 #include <string>
 #include <sstream>
 
 int main()
 {
-	std::string line;
-	std::getline(std::cin, line);
-	std::cout<<line<<std::endl;
-	std::istringstream iss(line);
+    std::string line;
+    std::getline(std::cin, line); //считываем строчку
+    std::istringstream iss(line); //из line создаем поток istringstream
 
+    std::string str;
+    int* arr = new int[1000]; //создаем стек
+    int *parr = &arr[0]; //создаем указатель на вершину стека
+    int i;
+    while(iss >> str) //читаем из строчки из потока
+    {
+        if(str == "+") {
+            *(parr-1) = *parr + *(parr-1);
+            parr -= 1;
+        } else if(str == "-") {
+            *(parr-1) = *(parr-1) - *parr;
+            parr -= 1;
+        } else if(str == "*") {
+            *(parr-1) = *parr * *(parr-1); //производим бинарные операции и смещаем указатель
+            parr -= 1;
+        } else if(str == "/") {
+            *(parr-1) = *(parr-1) / *parr;
+            parr -= 1;
+        } else {
+            parr += 1; //смещаем указатель
+            *parr = stoi(str); //записыываем результат операции в стек
+        }
+    }
+    std::cout<<*parr; //выводим конечный результат
+    delete [] arr; //очищаем память
 
-	std::string str;
-
-	int* arr = new int[1000];
-	int *parr = &arr[0];
-	int i;
-	while(iss >> str)
-	{
-	std::cout<<str<<std::endl;
-		if(str == "+") {
-			*(parr-1) = *parr + *(parr-1);
-			parr -= 1;
-		} else if(str == "-") {
-                        *(parr-1) = *(parr-1) - *parr;
-			parr -= 1;
-		} else if(str == "*") {
-                        *(parr-1) = *parr * *(parr-1);
-			parr -= 1;
-		} else if(str == "/") {
-                        *(parr-1) = *(parr-1) / *parr;
-			parr -= 1;
-		} else {
-			parr += 1;
-			*parr = stoi(str);
-			std::cout<<str<<std::endl;
-		}
-	}
-	std::cout<<*parr;
-	delete [] arr;
-
-	return 0;
+    return 0;
 }
