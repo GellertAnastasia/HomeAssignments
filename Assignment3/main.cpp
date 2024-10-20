5313f37ca79f9453e16b5714e3336031bf7abdd8
GellertAnastasia <st130144@student.spbu.ru>
2024-10-21 00:54:53 +0300

    creating README

--- Assignment3/main.cpp
+++ Assignment3/main.cpp
@@ -4,16 +4,4 @@
 #include "airbot.h"
 
 int main() {
-	Airbot airbot(4,7);
-	airbot.SetAmmo(50);
-	airbot.fire();
-	airbot.SetFuel(100);
-	Sword sword;
-        airbot.strike(sword);
-        //airbot.fight();
-	std::cout<<airbot.GetAmmo()<<std::endl;
-	std::cout<<airbot.GetFuel()<<std::endl;
-	std::cout<<airbot.GetSpeed()<<std::endl;
-	std::cout<<airbot.GetStrength()<<std::endl;
-	return 0;
 }
