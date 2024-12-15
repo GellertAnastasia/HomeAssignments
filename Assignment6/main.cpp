00da35da09c0b9ee2037aa2d36dd1622d32deddc
GellertAnastasia <st130144@student.spbu.ru>
2024-12-15 21:34:46 +0300

    creating tests and makefile

--- Assignment6/main.cpp
+++ Assignment6/main.cpp
@@ -3,16 +3,5 @@
 
 int main()
 {
-	Class<int> m(10, 5, {1.0f, 2.0f});
-	std::cout<<m.foo()<<std::endl;
-
-	Class<double> n(10.89, 5, {1.0f, 2.0f});
-	std::cout<<n.foo()<<std::endl;
-
-	Class1 x;
-	Class<Class1> p(x, 5, {1.0f, 2.0f});
-	std::cout<<p.foo()<<std::endl;
-
-
 	return 0;
 }
---
+++ Assignment6/test.cpp
@@ -0,0 +1,62 @@
+#include <gtest/gtest.h> 
+#include "class.h"
+#include "obj.h"
+
+
+int main(int argc, char **argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
+
+TEST(specialization, testInt) 
+{
+    Class<int> n(1, 1, {}); 
+    EXPECT_TRUE(n.foo()); 
+}
+TEST(specialization, testDouble) 
+{
+    Class<double> n(1.1, 1, {}); 
+    EXPECT_FALSE(n.foo()); 
+} 
+
+
+TEST(Class1, True) 
+{ 
+    Class1 obj; 
+    Class<Class1> n(obj, 1, {}); 
+    EXPECT_TRUE(n.foo()); 
+}
+TEST(Class1, False) 
+{ 
+    Class1 obj; 
+    Class<Class1> n(obj, 0, {}); 
+    EXPECT_FALSE(n.foo()); 
+} 
+ 
+TEST(Class2, True) 
+{ 
+    Class2 obj; 
+    Class<Class2> n(obj, 0, {1, 2}); 
+    EXPECT_TRUE(n.foo()); 
+} 
+TEST(Class2, False) 
+{ 
+    Class2 obj;
+    Class<Class2> n(obj, 0, {}); 
+    EXPECT_FALSE(n.foo()); 
+}
+ 
+TEST(Class3, True) 
+{ 
+    Class3 obj; 
+    Class<Class3> n(obj, -3, {1, 2, 3}); 
+    EXPECT_TRUE(n.foo()); 
+}
+TEST(Class3, False) 
+{ 
+    Class3 obj;
+    Class<Class3> n(obj, -2, {1, 2, 3}); 
+    EXPECT_FALSE(n.foo()); 
+} 
+
